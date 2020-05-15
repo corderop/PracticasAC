b0ba2daa8ae34fd9a7cd7495c6b4e386a84d3f84
Pablo Cordero <pablocorderoromero@gmail.com>
2020-05-15 20:47:38 +0200

    BP4 - Unidas las modificaciones

---
+++ bp4/ejer1/figura/figura1_ab.c
@@ -0,0 +1,51 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+struct {
+	int a;
+	int b;
+} s[5000];
+
+int main() {
+	int i, ii;
+	int X1, X2;
+	int R[40000];
+	clock_t t1, t2;
+	double total;
+
+	for(i=0; i<5000; i++){
+		s[i].a = 1;
+		s[i].b = 1;
+	}
+
+	t1 = clock();
+
+	for (ii=0; ii<40000;ii++) {
+		X1=0; X2=0; 
+		
+		for(i=0; i<5000; i+=5) {
+			X1+=2*s[i].a+ii;
+			X1+=2*s[i+1].a+ii;
+			X1+=2*s[i+2].a+ii;
+			X1+=2*s[i+3].a+ii;
+			X1+=2*s[i+4].a+ii;
+
+			X2+=3*s[i].b-ii;
+			X2+=3*s[i+1].b-ii;
+			X2+=3*s[i+2].b-ii;
+			X2+=3*s[i+3].b-ii;
+			X2+=3*s[i+4].b-ii;
+		}
+
+		if (X1<X2) R[ii]=X1; else R[ii]=X2;
+	}
+
+	t2 = clock();
+	total = (double)(t2-t1)/CLOCKS_PER_SEC;
+
+	printf(" Tiempo: %f\n", total);
+	printf("R[0] = %d\nR[39999] = %d\n", R[0], R[39999]);
+
+	return 0;
+}
---
+++ bp4/ejer1/matrices/pmm_1_ab.c
@@ -0,0 +1,76 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+#define MAX 11585
+
+double m1[MAX][MAX], m2[MAX][MAX], m3[MAX][MAX]; 
+
+int main(int argc, char **argv)  {
+	int i, j, k;
+	clock_t t1, t2;
+	double total;
+
+	if(argc < 2){
+		fprintf(stderr,"Falta tamaño de la matriz (debe de ser cuadrada)\n");
+		exit(-1);
+	}
+
+	// Tamaño de la matriz pasado por parámetro
+	unsigned int tam = atoi(argv[1]);
+
+	if(tam < 2 || tam>MAX){
+		fprintf(stderr, "El tamaño no puede ser menor a 2 y mayor que ");
+		exit(-1);
+	}
+
+	// Inicializamos la matriz y los vectores
+	for(i=0; i<tam; i++){
+		for(j=0; j<tam; j++){
+			m1[i][j] = 0;
+			m2[i][j] = 2;
+			m3[i][j] = 2;
+		}
+	}
+
+	// Inicializamos la primera variable de tiempo
+	t1 = clock();
+
+	// Calculamos el producto m1 = m2*m3
+	for(i=0; i<tam; i++){
+		for(k=0; k<tam; k+=5){
+			for(j=0; j<tam; j++){
+			m1[i][j] += (m2[i][k]*m3[k][j]);
+			m1[i][j] += (m2[i][k+1]*m3[k+1][j]);
+			m1[i][j] += (m2[i][k+2]*m3[k+2][j]);
+			m1[i][j] += (m2[i][k+3]*m3[k+3][j]);
+			m1[i][j] += (m2[i][k+4]*m3[k+4][j]);
+			}
+		}
+	}
+
+	// Obtenemos el tiempo total transcurrido
+	t2 = clock();
+	total = (double)(t2-t1)/CLOCKS_PER_SEC;
+
+	// Para tamaños pequeños (hasta tam=11), imprimimos todas las componentes del vector resultante
+	// y el tiempo de ejecución
+	if(tam <= 11){
+		printf("Tamaño matriz: %i\n Tiempo de ejecución: %f\n", tam, total);
+
+		for(i=0; i<tam; i++){
+			for(j=0; j<tam; j++){
+				printf("m1[%i][%i] = %f\n", i, j, m1[i][j]);
+			}
+		}
+	}
+
+	// Para tamaños superiores, imprimimos el tiempo de ejecución y la primera y última componente del vector
+	else{
+		printf("Tamaño vectores: %i\n Tiempo de ejecución: %f\n Primera componente: %f\n Última componente: %f\n",
+		tam, total, m1[0][0], m1[tam-1][tam-1]);
+	}
+
+	return 0;
+}
+
